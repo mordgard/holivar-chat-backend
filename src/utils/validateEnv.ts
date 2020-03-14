4e07bb0b8ddebfd917c50338e55733e3d31ae86d
Mikhail Sharakou <msharakov@gmail.com>
2020-03-14 20:19:28 +0300

    install jwt package and set jwt_secret into .env variable

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -2,11 +2,12 @@ import { cleanEnv, port, str } from "envalid";
 
 function validateEnv() {
   cleanEnv(process.env, {
+    JWT_SECRET: str(),
     MONGO_PASSWORD: str(),
     MONGO_PATH: str(),
     MONGO_USER: str(),
     NODE_ENV: str(),
-    PORT: port()
+    PORT: port(),
   });
 }
 
