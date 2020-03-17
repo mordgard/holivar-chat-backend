bb1d9ae036d07bc45b3defb1fcddb9c4f482586c
Mikhail Sharakou <msharakov@gmail.com>
2020-03-17 23:22:06 +0300

    clean up routes

---
+++ backend/src/routes.ts
@@ -0,0 +1,7 @@
+import { router as authRoutes } from "./components/auth";
+import { router as topicsRoutes } from "./components/topics";
+import { router as userRoutes } from "./components/user";
+
+const routes = { topicsRoutes, userRoutes, authRoutes };
+
+export { routes };
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 import bodyParser from "body-parser";
 import express from "express";
-import { topicRoutes } from "./components/topics";
-import { userRoutes } from "./components/user";
 import { connectToDb } from "./database/db";
+import { routes } from "./routes";
 
 const app: express.Application = express();
 
@@ -10,7 +9,8 @@ connectToDb();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/api/v1/topics/", topicRoutes);
-app.use("/api/v1/users/", userRoutes);
+app.use("/api/v1/topics/", routes.topicsRoutes);
+app.use("/api/v1/users/", routes.userRoutes);
+app.use("/api/v1/", routes.authRoutes);
 
 export { app };
