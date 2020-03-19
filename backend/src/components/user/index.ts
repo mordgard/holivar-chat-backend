d06a13084153cb3020ce480564f8339adaeb06e0
Mikhail Sharakou <msharakov@gmail.com>
2020-03-19 22:00:53 +0300

    update user component, add auth middlewares

--- backend/src/components/user/index.ts
+++ backend/src/components/user/index.ts
@@ -1,2 +1,3 @@
 export * from "./routes";
 export * from "./service";
+export * from "./interfaces";
---
+++ backend/src/components/user/interfaces.ts
@@ -0,0 +1,7 @@
+export interface IUserRole {
+  role: "moderator" | "admin";
+}
+
+export interface IUserStatus {
+  status: "new" | "active" | "archived";
+}
