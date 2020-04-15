a3df4b4f3fe41ed5dbe79392ad5e1739b748bab9
Mikhail Sharakou <msharakov@gmail.com>
2020-04-15 07:28:51 +0300

    install bcrypt + use express.json() instead body-parser

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-import bodyParser from "body-parser";
 import express from "express";
 import { connectToDb } from "./database/db";
 import { routes } from "./routes";
@@ -7,7 +6,7 @@ const app: express.Application = express();
 
 connectToDb();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use("/api/v1/topics/", routes.topicsRoutes);
 app.use("/api/v1/users/", routes.userRoutes);
