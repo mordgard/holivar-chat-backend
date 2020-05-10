ef1f16d419795e873cb09dfdd5e1f024fe430c63
Mikhail Sharakou <msharakov@gmail.com>
2020-05-10 21:02:37 +0300

    test sockets

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { app } from "./app";
 import { config } from "./config";
+import { server } from "./server";
 import { logger } from "./utils/logger";
 
-app.listen(config.port, () => {
+server.listen(config.port, () => {
   logger.info(`Server started at localhost:${config.port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-// 💩
+import http from "http";
+import socketIO from "socket.io";
+import { app } from "./app";
+import { logger } from "./utils";
+
+const server: http.Server = http.createServer(app);
+const io: socketIO.Server = socketIO(server);
+
+io.on("connection", socket => {
+  logger.info(`New connection: ${socket.id}`);
+});
+
+export { server };
