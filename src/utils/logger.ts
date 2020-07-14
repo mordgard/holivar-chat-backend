80d66a55db567dca15aefbeb224a28388a6ed23f
Mikhail Sharakou <msharakov@gmail.com>
2020-07-14 22:39:36 +0300

    add prettierrc and fix logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,15 +3,13 @@ import winston from "winston";
 
 const logger = winston.createLogger({
   format: winston.format.combine(
-    winston.format.label({ label: path.basename(process.mainModule.filename) }),
+    winston.format.label({ label: path.basename(__filename) }),
     winston.format.colorize(),
     winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-    winston.format.printf(
-      info => `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`
-    )
+    winston.format.printf(info => `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`),
   ),
   level: "debug",
-  transports: [new winston.transports.Console()]
+  transports: [new winston.transports.Console()],
 });
 
 export { logger };
